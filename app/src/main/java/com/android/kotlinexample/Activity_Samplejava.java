122696b889b3a41c6626dd22f7cd276ab5198b62
Android <janardhan.srit@gmail.com>
2019-10-14 18:47:25 +0530

    S commit

--- app/src/main/java/com/android/kotlinexample/Activity_Samplejava.java
+++ app/src/main/java/com/android/kotlinexample/Activity_Samplejava.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class Activity_Samplejava extends AppCompatActivity {
 
-//Testing sample third commit
+
 
 
 
@@ -14,6 +14,9 @@ public class Activity_Samplejava extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_samplejava);
+
+
+
 
     }
 }
