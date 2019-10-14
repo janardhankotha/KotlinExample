1f9e7a7255b3ec0f09b4be07f319dab59b885858
Android <janardhan.srit@gmail.com>
2019-10-14 18:47:44 +0530

    S commit

--- app/src/main/java/com/android/kotlinexample/Activity_Samplejava.java
+++ app/src/main/java/com/android/kotlinexample/Activity_Samplejava.java
@@ -7,9 +7,6 @@ import android.os.Bundle;
 public class Activity_Samplejava extends AppCompatActivity {
 
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
